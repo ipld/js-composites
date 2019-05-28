4c6adc4c7db35f7842f7013cfa9c83dc413bef7e
Mikeal Rogers <mikeal.rogers@gmail.com>
2019-05-28 19:17:56 +0000

    fix: basic traversal

--- index.js
+++ index.js
@@ -16,16 +16,29 @@ const Type = withIs(BaseType, {
 
 class MapKind extends Type {
   get (args) {
-    if (!args.attr) throw new Error('Missing required argument "attr"')
-    let attr = args.attr
-    if (!this.value[attr]) throw new Error(`Not Found. Node has no attribute "${attr}"`)
-    return { result: this.value[attr] }
+    if (!args.path) throw new Error('Missing required argument "path"')
+    let path = args.path.split('/').filter(x => x)
+    let value = this.node
+    while (path.length) {
+      let attr = path.shift()
+      if (typeof value[attr] === 'undefined') throw new Error(`Not Found. Node has no attribute "${attr}"`)
+      value = value[attr]
+      if (CID.isCID(value)) {
+        return { call: { method: 'get', args: { path: path.join('/') } }, target: value, proxy: true }
+      }
+    }
+    return { result: value }
   }
 }
 
 class LinkKind extends Type {
   resolve (args) {
     return { result: this.node }
+  }
+}
+class StringKind extends Type {
+  toString (args) {
+    return this.node
   }
 }
 
@@ -39,6 +52,7 @@ class Lookup {
     if (source._type) return this.fromNode(source)
     if (CID.isCID(source)) return new LinkKind(source)
     if (source && typeof source === 'object') return new MapKind(source)
+    if (typeof source === 'string') return new StringKind(source)
     throw new Error('NOT IMPLEMENTED!')
   }
 }
@@ -76,15 +90,20 @@ const system = async function * (opts, target, info = {}) {
     let response = await target[info.method](info.args)
     yield { response, target, call: info, result: response.result }
     if (response.call) {
-      info.continuation = response.continuation || {}
-      let last
-      for await (let trace of system(opts, response.result, response.call)) {
-        last = trace
-        yield trace
+      if (response.proxy) {
+        yield * system(opts, response.target, response.call)
+      } else {
+        info.continuation = response.continuation || {}
+        let last
+        for await (let trace of system(opts, response.target, response.call)) {
+          last = trace
+          yield trace
+        }
+        info.continuation.response = last
+        yield * system(opts, target, info)
       }
-      info.continuation.response = last
-      yield * system(opts, target, info)
-    } else if (response.result) {
+    } 
+    if (response.result) {
       yield * system(opts, response.result)
     }
   }
---
+++ test/kinds.spec.js
@@ -0,0 +1,67 @@
+const Block = require('@ipld/block')
+const assert = require('assert')
+const tsame = require('tsame')
+const { system, Lookup } = require('../')
+
+const same = (...args) => assert.ok(tsame(...args))
+const test = it
+
+const storage = () => {
+  let kv = {}
+  let get = cid => {
+    let _cid = cid.toBaseEncodedString()
+    if (!kv[_cid]) throw new Error('Not found.')
+    return kv[_cid]
+  }
+  let put = async block => {
+    let cid = await block.cid()
+    let _cid = cid.toBaseEncodedString()
+    kv[_cid] = block
+  }
+  return {put, get}
+}
+
+const getResult = async iter => {
+  let last
+  for await (let part of iter) {
+    last = part
+  }
+  return last.result
+}
+
+const asyncList = async iter => {
+  let parts = []
+  for await (let part of iter) {
+    parts.push(part)
+  }
+  return parts
+}
+
+const lookup = new Lookup()
+
+const _getCall = path => ({ method: 'get', args: { path } })
+
+test('single block traversal', async () => {
+  let block = Block.encoder({hello: 'world'}, 'dag-json')
+  let result = await getResult(system({lookup}, block, _getCall('hello')))
+  let str = result.toString()
+  same(str, 'world')
+  
+  block = Block.encoder({one: {two: 'world'}}, 'dag-json')
+  result = await getResult(system({lookup}, block, _getCall('one/two')))
+  str = result.toString()
+  same(str, 'world')
+})
+
+test('multi-block traversal', async () => {
+  let leaf = Block.encoder({hello: 'world'}, 'dag-json')
+  let child = Block.encoder({two: await leaf.cid()}, 'dag-json')
+  let root = Block.encoder({one: await child.cid()}, 'dag-json')
+  let { get, put } = storage()
+  await Promise.all([leaf, child, root].map(block => put(block)))
+  let iter = system({lookup, get}, root, _getCall('one/two/hello'))
+  let result = await getResult(iter)
+  let str = result.toString()
+  same(str, 'world')
+})
+  
